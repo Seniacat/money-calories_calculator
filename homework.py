edec4c6779c44f4d7bebc33b82fae0ed7021fe0c
Ksenia <senya.sidorovich@gmail.com>
2021-07-30 14:01:39 +0300

    First correction

--- homework.py
+++ homework.py
@@ -1,5 +1,5 @@
 import datetime as dt
-from typing import Optional, Union
+from typing import Optional
 
 
 class Record:
@@ -12,13 +12,10 @@ class Record:
         self.comment = comment
         self.date = (
             dt.datetime.strptime(date, '%d.%m.%Y').date()
-            if date else Calculator.current_date)
+            if date else dt.datetime.now().date())
 
 
 class Calculator:
-    current_date = dt.datetime.now().date()
-    cut_off_date = current_date - dt.timedelta(weeks=1)
-
     def __init__(self, limit: int) -> None:
         self.limit = limit
         self.records: list = []
@@ -27,34 +24,41 @@ class Calculator:
         self.records.append(record)
 
     def get_today_stats(self) -> float:
-        return sum([record.amount for record in self.records
-                    if record.date == Calculator.current_date])
+        return sum(
+            record.amount for record in self.records
+            if record.date == dt.datetime.now().date())
 
     def get_today_remained(self) -> float:
         return self.limit - self.get_today_stats()
 
     def get_week_stats(self) -> float:
-        return sum([record.amount for record in self.records
-                    if record.date <= Calculator.current_date
-                    and record.date > Calculator.cut_off_date])
+        current_date = dt.datetime.now().date()
+        cut_off_date = dt.datetime.now().date() - dt.timedelta(weeks=1)
+        return sum(
+            record.amount for record in self.records
+            if record.date <= current_date
+            and record.date > cut_off_date)
 
 
 class CashCalculator(Calculator):
     USD_RATE = 73.25
     EURO_RATE = 86.96
 
-    def get_today_cash_remained(self, currency: str) -> Union[float, str]:
+    RATES = {
+        'usd': ('USD_RATE', 'USD'),
+        'eur': ('EURO_RATE', 'Euro')
+    }
+
+    def get_today_cash_remained(self, currency: str) -> str:
         if currency == 'rub':
             cash_remained = round(super().get_today_remained(), 2)
             value = 'руб'
-        elif currency == 'usd':
-            cash_remained = round(
-                super().get_today_remained() / CashCalculator.USD_RATE, 2)
-            value = 'USD'
         else:
+            divider = getattr(
+                CashCalculator, CashCalculator.RATES.get(currency)[0])
             cash_remained = round(
-                super().get_today_remained() / CashCalculator.EURO_RATE, 2)
-            value = 'Euro'
+                super().get_today_remained() / divider, 2)
+            value = CashCalculator.RATES.get(currency)[1]
         if cash_remained > 0:
             return f'На сегодня осталось {cash_remained} {value}'
         elif cash_remained == 0:
@@ -65,10 +69,10 @@ class CashCalculator(Calculator):
 
 
 class CaloriesCalculator(Calculator):
-    def get_calories_remained(self) -> Union[float, str]:
+    def get_calories_remained(self) -> str:
         calories_remained = super().get_today_remained()
         allow_msg = (
-            f'Сегодня можно съесть что-нибудь ещё, но с'
+            'Сегодня можно съесть что-нибудь ещё, но с'
             f' общей калорийностью не более {calories_remained} кКал'
         )
         stop_msg = 'Хватит есть!'
